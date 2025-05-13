41d807ebef69d40519170b4292767dc3bcf32c57
Equilibretech <partenaires@equilibretech.com>
2025-05-13 18:49:43 +0200

    Update ReactFlowSynoptic.jsx

--- src/ReactFlowSynoptic.jsx
+++ src/ReactFlowSynoptic.jsx
@@ -41,14 +41,19 @@ const ReactFlowSynoptic = ({ roomId }) => {
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
   useEffect(() => {
-    fetch(`https://x8ki-letl-twmt.n7.xano.io/api:Dosppuhb/get_graph_by_room_id?room_id=${roomId}`)
-      .then(res => res.json())
-      .then(json => {
-        const layouted = getLayoutedElements(json.nodes, json.edges);
-        setNodes(layouted.nodes);
-        setEdges(layouted.edges);
-      });
-  }, [roomId, setNodes, setEdges]);
+  console.log("Chargement du roomId :", roomId);
+  setNodes([]); // reset immédiat
+  setEdges([]);
+
+  fetch(`https://x8ki-letl-twmt.n7.xano.io/api:Dosppuhb/get_graph_by_room_id?room_id=${roomId}`)
+    .then(res => res.json())
+    .then(json => {
+      console.log("Données reçues :", json); // debug
+      const layouted = getLayoutedElements(json.nodes, json.edges);
+      setNodes(layouted.nodes);
+      setEdges(layouted.edges);
+    });
+}, [roomId, setNodes, setEdges]);
 
   return (
     <div style={{ height: '100vh', width: '100%' }}>

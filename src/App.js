5b412c7c3268ccb7cc107702ad37dbed77fe4ab6
Equilibretech <partenaires@equilibretech.com>
2025-05-13 18:26:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactFlowSynoptic from './ReactFlowSynoptic';
 
 function App() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const roomId = urlParams.get("roomId") || "3aa1b1cb-361f-4451-93b9-12c28b241173"; // fallback utile en dev local
+
+  console.log("roomId reçu :", roomId); // debug temporaire
+
   return (
     <div style={{ height: '100vh' }}>
-      {/* Remplace "test-room" par un room_id réel si tu en as un */}
-      <ReactFlowSynoptic roomId="3aa1b1cb-361f-4451-93b9-12c28b241173" />
+      <ReactFlowSynoptic roomId={roomId} />
     </div>
   );
 }
